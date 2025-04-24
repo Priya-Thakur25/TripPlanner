fdc71d693e7992864ae91a1c54dc1f440275ef1e
Priya <priyathakur25102003@gmail.com>
2025-04-24 14:22:23 +0530

    adding

--- src/view-trip/[tripId]/view-trip-components/PlaceToVisit.jsx
+++ src/view-trip/[tripId]/view-trip-components/PlaceToVisit.jsx
@@ -7,7 +7,7 @@ function PlaceToVisit({ trip }) {
       <h2 className="text-xl font-bold mt-5">Places to Visit</h2>
       {trip?.tripData?.itinerary.map((item,index) => (
         <div key={index} className='p-2'>
-          <h2 className='font-medium text-lg'>Day {item.day}</h2>
+          <h2 className='font-medium text-lg'>{item.day}</h2>
           <div className='grid md:grid-cols-2 gap-5'>
             {item?.plan.map((it,index) => (
               <div className='' key={index}>
